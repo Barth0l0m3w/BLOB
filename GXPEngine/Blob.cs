84b5b82fbe38f8d4472b3309441437ec85ac1e4d
Bulancea Matei <67746218+Oberonjr@users.noreply.github.com>
2022-02-16 14:30:06 +0100

    PlatformBounceImplemented v1.0

--- GXPEngine/Blob.cs
+++ GXPEngine/Blob.cs
@@ -26,10 +26,13 @@ public class Blob : Sprite
         StartGame();
         //Bounce();
 
+        /*
         x += speedX * dirX;
         y += speedY * dirY;
+        */
 
-        
+        Move(speedX * dirX, 0);
+        Move(0, speedY * dirY);
 
         if (y > game.height - height)
         {
@@ -45,6 +48,9 @@ public class Blob : Sprite
         {
             speedX = Utils.Random(-2, 2.1f);
             speedY = 2.0f;
+
+            dirX = 1.0f;
+            dirY = 1.0f;
 
         }
     }
@@ -94,10 +100,27 @@ public class Blob : Sprite
         }
         if (other is Board)
         {
+            
             board = other as Board;
-            if(board.x > this.x){
-                Console.WriteLine(board.x - this.x);
+            float xOffset = Mathf.Abs((board.x - this.x)) / 160.0f;      //214.0f;
+            speedX = 2 + xOffset;
+            speedY = 2 - xOffset;
+            if (board.x > this.x){
+               dirX = -1;
             }
+            if (board.x < this.x)
+            {
+                dirX = 1;
+            }
+
+            /*
+            Console.WriteLine(board.x - this.x);
+            Console.WriteLine(board.width / 2 + this.width);
+            Console.WriteLine("X offest is: {0}", xOffset);
+            */
+
+            //dirX *= -1;
+            dirY = -1;
         }
 
         
@@ -111,28 +134,7 @@ public class Blob : Sprite
         speedY = 0;
     }
 
-    void Bounce() {
-        if (y < 0 - height)
-        {
-            dirY *= -1;
-        }
-
-        if (y > game.height + height)
-        {
-            Respawn();
-        }
-
-        if (x < 0 - width / 2)
-        {
-            dirX *= -1;
-        }
-
-        if (x > game.width - width)
-        {
-            dirX *= -1;
-        }
-
-    }
+    
 
 }
 
